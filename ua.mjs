28e8bd762113dee88b5870c09b954d3d76b79c09
Yury Semikhatsky <yurys@chromium.org>
2023-05-06 00:27:26 -0200

    Custom UA

--- ua.mjs
+++ ua.mjs
@@ -1,9 +1,8 @@
-import { chromium, webkit } from '@playwright/test';
+import { webkit } from '@playwright/test';
 
 (async () => {
   const context = await webkit.launchPersistentContext('my-data', {
-    headless: false,
-    // userAgent: 'CustomUA'
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604'
   });
   const page = await context.newPage();
   console.log('user-agent = ', await page.evaluate(() => navigator.userAgent));
